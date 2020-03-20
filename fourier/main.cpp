202ffdff2ee728f26274d8b51db6f54005ec75ce
Harshit Maurya <hmaurya999@gmail.com>
2020-03-20 13:49:37 +0530

    basic demonstration to find frequency

--- fourier/main.cpp
+++ fourier/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+#include "DFT.h"
+#include "WaveFactory.h"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ComplexNumberSeries timeDomainWave = WaveFactory::generateSineWaveComplex(300, 1, 800, 1);
+    DFT dft(timeDomainWave);
+    double maxValue = 0;
+    int maxFrequency = 0;
+    auto transformValues = dft.getFourierTransform();
+    for (int idx = 0; idx < transformValues.size(); idx++) {
+        ComplexNumber num = transformValues.at(idx);
+        double frequencyDomainValue = abs(num);
+        if (frequencyDomainValue > maxValue) {
+            maxValue = frequencyDomainValue;
+            maxFrequency = idx;
+        }
+    }
+    std::cout << maxFrequency;
     return 0;
 }
